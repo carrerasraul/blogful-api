08bc50e4367ac07496e41f1dcde2137b4a5196da
Raul Carreras <raul.e.carreras@gmail.com>
2020-03-05 19:00:11 -0500

    update

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ const { NODE_ENV } = require('./config')
 
 const app = express()
 
-const morganOption = (process.env.NODE_ENV === 'production')
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
